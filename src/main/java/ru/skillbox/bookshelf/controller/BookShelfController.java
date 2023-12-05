b8bc265715021f942efc30b2629f76da3bc1b91b
Abriel <abrielz11@gmail.com>
2023-12-05 13:38:04 +0300

    controller was created

--- src/main/java/ru/skillbox/bookshelf/controller/BookShelfController.java
+++ src/main/java/ru/skillbox/bookshelf/controller/BookShelfController.java
@@ -6,11 +6,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import ru.skillbox.bookshelf.dto.BookNewDto;
 import ru.skillbox.bookshelf.dto.BookResponseDto;
 import ru.skillbox.bookshelf.service.BookShelf;
 import java.util.List;
@@ -50,9 +56,31 @@ public class BookShelfController {
 
     //TODO: создать книгу
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookResponseDto createBook(@RequestBody BookNewDto bookNewDto) {
+
+        //return bookShelfService.createBook(bookNewDto);
+        return null;
+    }
 
     //TODO: обновить информацию о книге
 
-    
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public BookResponseDto updateBookById(@PathVariable(name = "id") Long id,
+                                      @RequestBody BookResponseDto bookResponseDto) {
+
+        //return bookShelfService.createBookById(id, bookResponseDto);
+        return null;
+    }
+
+
     //TODO: удалить книгу по ID
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBookById(@PathVariable(name = "id") Long id) {
+        //bookShelfService.deleteBookById(id);
+    }
 }
