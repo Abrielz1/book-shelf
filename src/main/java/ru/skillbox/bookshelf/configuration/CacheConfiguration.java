356f8c018d19a3c993b1579a18ff9784251c2a3c
Abriel <abrielz11@gmail.com>
2023-12-15 23:46:20 +0300

    application redis was nearly done adding

---
+++ src/main/java/ru/skillbox/bookshelf/configuration/CacheConfiguration.java
@@ -0,0 +1,73 @@
+package ru.skillbox.bookshelf.configuration;
+
+import com.google.common.cache.CacheBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import ru.skillbox.bookshelf.configuration.properties.AppCacheProperties;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+@EnableConfigurationProperties(AppCacheProperties.class)
+public class CacheConfiguration {
+
+    @Bean
+    @ConditionalOnExpression("'${app.cache.cacheType}'.equals('inMemory')")
+    public ConcurrentMapCacheManager inMemoryCacheManager(AppCacheProperties properties) {
+
+        var cacheManager = new ConcurrentMapCacheManager(){
+
+        @Override
+        protected Cache createConcurrentMapCache(String name) {
+            return new ConcurrentMapCache(name,
+                    CacheBuilder.newBuilder()
+                            .expireAfterWrite(properties.getCachePropertiesMap().get(name).getExpiry())
+                            .build()
+                            .asMap(),
+                    true
+            );
+            }
+        };
+
+        var cacheNames = properties.getCacheNames();
+
+        if (!cacheNames.isEmpty()) {
+            cacheManager.setCacheNames(cacheNames);
+        }
+
+        return cacheManager;
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "app.redis", name = "enable", havingValue = "true")
+    @ConditionalOnExpression("'${app.cache.cacheType}'.equals('redis')")
+    public CacheManager redisCacheManager(AppCacheProperties appCacheProperties,
+                                          LettuceConnectionFactory lettuceConnectionFactory) {
+
+        var defaultConfig = RedisCacheConfiguration.defaultCacheConfig();
+        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
+
+        appCacheProperties.getCacheNames().forEach(cacheNames -> {
+            redisCacheConfigurationMap.put(cacheNames, RedisCacheConfiguration.defaultCacheConfig().entryTtl(
+                    appCacheProperties.getCachePropertiesMap().get(cacheNames).getExpiry()
+            ));
+        });
+
+        return RedisCacheManager.builder(lettuceConnectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(redisCacheConfigurationMap)
+                .build();
+    }
+}
